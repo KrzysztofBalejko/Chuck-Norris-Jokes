89a02d5a9274d8fbecb66b76dba15e0fec7c7c0b
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-29 17:20:34 +0000

    Adding alert

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 // Using external API
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
+  if (number === '') {
+    alert('You forgot to enter a number!')
+  } else if (number === '0') {
+    alert('Please enter a number greater than zero!')
+  }
 
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `https://api.icndb.com/jokes/random/${number}`,true);
